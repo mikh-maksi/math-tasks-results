40d803701e591721ee0735e4c6fdad4f98136cfa
mikh-maksi <newlevelcenter@gmail.com>
2024-10-04 22:20:27 +0300

    OP2

--- op/op2/op2.php
+++ op/op2/op2.php
@@ -3,13 +3,11 @@ header("Content-type: image/png");
 
 $assets_path = "../../assets";
 
-
-
 // Массиви з групами
 $names['activities'] = ['football', 'going-to-school', 'walking-with-dog'];
 $names['animals'] = ['bird', 'cat', 'dino', 'dog', 'duck', 'fish'];
 $names['balls'] = ['ball-blue', 'ball-green', 'ball-red', 'ball-yellow'];
-$names['fruits'] = ['apple', 'apricot', 'banana', 'broccoli', 'carrot', 'grape', 'kiwi', 'lemon', 'orange', 'pear', 'strawberry', 'tomato'];
+$names['fruits-vegetables'] = ['apple', 'apricot', 'banana', 'broccoli', 'carrot', 'grape', 'kiwi', 'lemon', 'orange', 'pear', 'strawberry', 'tomato'];
 $names['items'] = ['ball-beach', 'baloon', 'block', 'book', 'box', 'clay', 'dumbell', 'feather', 'flat', 'flower', 'house', 'mountain', 'paper', 'pencil', 'rubber', 'ruler', 'smartphone', 'star', 'string'];
 $names['sweets'] = ['cake', 'candy', 'cookie'];
 $names['vehicles'] = ['car-front', 'racing-car-blue', 'racing-car-green', 'racing-car-purple', 'racing-car-red', 'racing-car-yellow', 'simple-car-blue', 'simple-car-green', 'simple-car-purple', 'simple-car-red', 'simple-car-yellow'];
@@ -63,12 +61,12 @@ imagefill($back, 0, 0, $light_gray);
 // Функція для завантаження зображення об'єкта або дефолтного 'orange'
 function load_item_image($group_name, $image_name) {
     global $assets_path;
-    $item_image_path = $assets_path."/images/flat48/{$group_name}/{$image_name}.png"; // Шлях до зображення
+    $item_image_path = $assets_path."/images/flat96/{$group_name}/{$image_name}.png"; // Шлях до зображення
     if (file_exists($item_image_path)) {
         return imagecreatefrompng($item_image_path); // Завантажуємо зображення об'єкта
     } else {
         // Якщо зображення не знайдено, завантажуємо дефолтне зображення 'orange' з групи 'fruits'
-        $default_image_path = $assets_path."/images/flat48/fruits/orange.png";
+        $default_image_path = $assets_path."/images/flat96/fruits-vegetables/orange.png";
         if (file_exists($default_image_path)) {
             return imagecreatefrompng($default_image_path); // Завантажуємо дефолтне зображення
         } else {
@@ -79,7 +77,7 @@ function load_item_image($group_name, $image_name) {
 
 // Завантажуємо зображення об'єкта
 $item = load_item_image($group_name, $image_name);
-$delete_img = imagecreatefrompng($assets_path."/images/flat48/delete/delete.png"); // Завантажуємо зображення хрестика
+$delete_img = imagecreatefrompng($assets_path."/images/flat96/delete/delete.png"); // Завантажуємо зображення хрестика
 
 // Визначаємо гап (відстань) між об'єктами
 $gap = 11.5; 
@@ -88,6 +86,11 @@ $gap = 11.5;
 $original_item_width = imagesx($item);
 $original_item_height = imagesy($item);
 
+// $original_item_width = 48;
+// $original_item_height = 48;
+
+
+
 // Обчислюємо доступну ширину для об'єктів
 $available_width = $width - 2 * 35;
 
@@ -95,8 +98,11 @@ $available_width = $width - 2 * 35;
 $scale = min($available_width / $sub / $original_item_width, $height / 2 / $original_item_height);
 
 // Обчислюємо нові розміри зображення об'єкта
-$object_width = $original_item_width;
-$object_height = $original_item_height;
+// $object_width = $original_item_width;
+// $object_height = $original_item_height;
+
+$object_width = 48;
+$object_height = 48;
 
 // Позиції для кожного об'єкта
 $x_offset = ($width - ($sub * $object_width + ($sub - 1) * $gap)) / 2; // Центруємо по горизонталі
